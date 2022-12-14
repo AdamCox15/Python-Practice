aff6eb163e27ba74b875db28075279b8d3b2b4d4
Adam Cox <adamcox.dev@gmail.com>
2022-12-13 17:57:16 -0700

    saving string method practice

--- strings.py
+++ strings.py
@@ -137,8 +137,23 @@ winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the
 winter_trees_full = '\n'.join(winter_trees_lines)
 print(winter_trees_full)
 
+love_maybe_lines = ['Always    ', '     in the middle of our bloodiest battles  ', 'you lay down your arms', '           like flowering mines    ','\n' ,'   to conquer me home.    ']
 
+love_maybe_lines_stripped = []
+for love in love_maybe_lines:
+  love_maybe_lines_stripped.append(love.strip())
 
+love_maybe_full = '\n'.join(love_maybe_lines_stripped)
+print(love_maybe_full)
 
+toomer_bio = \
+"""
+Nathan Pinchback Tomer, who adopted the name Jean Tomer early in his literary career, was born in Washington, D.C. in 1894. Jean is the son of Nathan Tomer was a mixed-race freedman, born into slavery in 1839 in Chatham County, North Carolina. Jean Tomer is most well known for his first book Cane, which vividly portrays the life of African-Americans in southern farmlands.
+"""
+toomer_bio_fixed = toomer_bio.replace('Tomer', 'Toomer')
+print(toomer_bio_fixed)
 
+god_wills_it_line_one = "The very earth will disown you"
 
+disown_placement = god_wills_it_line_one.find('disown')
+print(disown_placement)
