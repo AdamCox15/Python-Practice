dff3ecb5fbf9c42f2c7a1681c0c9404021269368
Adam Cox <adamcox.dev@gmail.com>
2022-12-20 16:43:45 -0700

    Practicing Dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -3,3 +3,7 @@ num_cameras = {"backyard": 6,  "garage": 2, "driveway": 1}
 
 print(sensors)
 print(num_cameras)
+
+translations = {"mountain": "orod", "bread": "bass", "friend": "mellon", "horse": "roch"}
+children = {"von Trapp":["Johannes", "Rosmarie", "Eleonore"],"Corleone":["Sonny", "Fredo", "Michael"]}
+my_empty_dictionary = {}
