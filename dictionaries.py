4585575770f11d802f7fedc01e3aa4f1b1c46ced
Adam Cox <adamcox.dev@gmail.com>
2022-12-20 17:21:54 -0700

    saving progress

--- dictionaries.py
+++ dictionaries.py
@@ -7,3 +7,33 @@ print(num_cameras)
 translations = {"mountain": "orod", "bread": "bass", "friend": "mellon", "horse": "roch"}
 children = {"von Trapp":["Johannes", "Rosmarie", "Eleonore"],"Corleone":["Sonny", "Fredo", "Michael"]}
 my_empty_dictionary = {}
+
+animals_in_zoo = {}
+animals_in_zoo["zebras"] = 8
+animals_in_zoo["monkeys"] = 12
+animals_in_zoo["dinosaurs"] = 0
+print(animals_in_zoo)
+
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+user_ids.update({"theLooper": 138475, "stringQueen": 85739})
+print(user_ids)
+
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+oscar_winners["Supporting Actress"] = "Viola Davis"
+oscar_winners["Best Picture"] = "Moonlight"
+print(oscar_winners)
+
+drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
+zipped_drinks = zip(drinks, caffeine)
+drinks_to_caffeine = {key:value for key, value in zip(drinks, caffeine)}
+
+songs = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
+playcounts = [78, 29, 44, 21, 89, 5]
+
+plays = {key:value for key, value in zip(songs, playcounts)}
+plays["Purple Haze"] = 1
+plays["Respect"] = 94
+print(plays)
+library = {"The Best Songs": plays, "Sunday Feelings": {}}
+print(library)
