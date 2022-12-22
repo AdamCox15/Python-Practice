4ba3413f79359a900f00259d45a0682a7c29333d
Adam Cox <adamcox.dev@gmail.com>
2022-12-21 18:31:38 -0700

    More dictionary practice

--- dictionaries.py
+++ dictionaries.py
@@ -71,9 +71,34 @@ health_points += available_items.pop("mystic bread", 0)
 print(available_items)
 print(health_points)
 
-  
-  
-  
-  
-  
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
 
+users = user_ids.keys()
+lessons = num_exercises.keys()
+print(users)
+print(lessons)
+  
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+total_exercises = 0
+for num in num_exercises.values():
+  total_exercises += num
+print(total_exercises)
+
+pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+for key, value in pct_women_in_occupation.items():
+  print("Women make up " + str(value) + " percent of " + key) 
+  
+tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+
+spread = {}
+spread["past"] = tarot.pop(13)
+spread["present"] = tarot.pop(22)
+spread["future"] = tarot.pop(10)
+
+for key, value in spread.items():
+  print("Your " + key + " is the " + value + " card.") 
+
+
+ 
