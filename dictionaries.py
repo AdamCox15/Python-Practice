a7e811b5c20ec028f4c25dbac77f4a796e6492f2
Adam Cox <adamcox.dev@gmail.com>
2022-12-21 18:02:22 -0700

    Saving progress

--- dictionaries.py
+++ dictionaries.py
@@ -50,6 +50,30 @@ if "energy" in zodiac_elements:
 
 zodiac_elements["energy"] = "Not a Zodiac element"
 
+caffeine_level = {"espresso": 64, "chai": 40, "decaf": 0, "drip": 120}
+caffeine_level["matcha"] = 30
+try:
+  print(caffeine_level["matcha"])
+except:
+  print("Unknown caffeine Level")
 
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+tc_id = user_ids.get("teraCoder", 10000)
+print(tc_id)
+stack_id = user_ids.get("superStackSmash", 100000)
+print(stack_id)
 
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
+health_points = 20
+health_points += available_items.pop("stamina grains", 0)
+health_points += available_items.pop("power stew", 0)
+health_points += available_items.pop("mystic bread", 0)
+print(available_items)
+print(health_points)
+
+  
+  
+  
+  
+  
 
