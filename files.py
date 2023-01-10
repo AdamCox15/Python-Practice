3101706b58a0ef09d266debc1385360a022885eb
Adam Cox <adamcox.dev@gmail.com>
2023-01-09 20:19:48 -0700

    Reading files and Dictionaries practice

--- files.py
+++ files.py
@@ -19,3 +19,19 @@ with open("bad_bands.txt", "w") as bad_bands_doc:
 ith open("cool_dogs.txt", "a") as cool_dogs_file:
   cool_dogs_file.write("Air Buddy")
   
+with open("fun_file.txt") as close_this_file:
+  setup = close_this_file.readline()
+  punchline = close_this_file.readline()
+
+  print(setup)
+  
+ with open("logger.csv") as log_csv_file:
+  print(log_csv_file.read())
+ 
+# ----------- Rading csv as a Dictionary -----------
+import csv
+
+with open("cool_csv.csv") as cool_csv_file:
+  cool_csv_dict = csv.DictReader(cool_csv_file)
+  for row in cool_csv_dict:
+    print(row['Cool Fact'])
