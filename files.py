38f773897bbc3f9691e8df52b1119b00f16d07bd
Adam Cox <adamcox.dev@gmail.com>
2023-01-09 19:58:30 -0700

    More files practice

--- files.py
+++ files.py
@@ -6,3 +6,16 @@ with open("how_many_lines.txt") as lines_doc:
   for line in lines_doc.readlines():
     print(line)
     
+with open("just_the_first.txt") as first_line_doc:
+  first_line = first_line_doc.readline()
+  print(first_line)
+
+#  ------------ Using the write method ----------
+with open("bad_bands.txt", "w") as bad_bands_doc:
+  bad_bands_doc.write("Jonas Brothers")
+  
+  
+# -------- Using the append method ------------
+ith open("cool_dogs.txt", "a") as cool_dogs_file:
+  cool_dogs_file.write("Air Buddy")
+  
