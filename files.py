6dbab5766d6dd49503f76d278c8ac2e3c7a281b4
Adam Cox <adamcox.dev@gmail.com>
2023-01-09 20:43:07 -0700

    Saving practice for the day

--- files.py
+++ files.py
@@ -59,3 +59,13 @@ import json
 with open('message.json') as message_json:
   message = json.load(message_json)
   print(message['text'])
+  
+#  --------------- Writing json files -------------
+data_payload = [
+  {'interesting message': 'What is JSON? A web application\'s little pile of secrets.',
+   'follow up': 'But enough talk!'}
+]
+
+import json
+with open('data.json', 'w') as data_json:
+  json.dump(data_payload, data_json)
