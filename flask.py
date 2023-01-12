fb1347897b74b7b842354009e58b9b584ff9a1c0
Adam Cox <adamcox.dev@gmail.com>
2023-01-11 23:29:33 -0700

    Saving flask practice

--- flask.py
+++ flask.py
@@ -6,5 +6,36 @@ app = Flask(__name__)
 
 print(__name__)
 
-# Routing 
+# Routing with render HTML
 
+@app.route('/')
+@app.route('/home')
+def home():
+    return '<h1>Hello, World!</h1>'
+
+
+@app.route('/reporter')
+def reporter():
+      return '''
+    <h2>Reporter Bio</h2>
+    <a href="/">Return to home page</a>
+    '''
+
+#   Routing with variable rules "Dynamic URLS"
+
+@app.route('/reporter/<int:reporter_id>')
+def reporter(reporter_id):
+    return '''
+    <h2>Reporter {reporter_id} Bio</h2>
+    <a href="/">Return to home page</a>
+    '''
+#  Adding a third view function
+
+@app.route('/article/<article_name>')
+def article(article_name):
+  return f'''
+  <h2>{article_name.replace('-', ' ').title()}</h2>
+  <a href='/'>Return back to home page</a>
+  '''
+  
+  
