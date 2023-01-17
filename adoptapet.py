61f3e25d308344253361f91521a2c27a123731e3
Adam Cox <adamcox.dev@gmail.com>
2023-01-16 19:29:10 -0700

    created a few routes

--- adoptapet.py
+++ adoptapet.py
@@ -1,1 +1,14 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return '''<h1>Adopt a Pet!</h1>
+  <p>Browse through the links below to find your new friend</P>
+  <ul><a href="/animals/dogs"><li>Dogs</li></a><a href ="/animals/cats"><li>Cats</li></a><a href ="/animals/rabbits"><li>Rabbits</li></a></ul>
+  '''
+
+@app.route('/animals/<pet_type>')
+def animals(pet_type):
+  return f'<h1>List of {pet_type}</h1>'
