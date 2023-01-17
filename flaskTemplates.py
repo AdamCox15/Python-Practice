dfab86d7f309b41a9161a7cee236de061ba303e9
Adam Cox <adamcox.dev@gmail.com>
2023-01-16 21:36:26 -0700

    Practice template variables

--- flaskTemplates.py
+++ flaskTemplates.py
@@ -30,7 +30,7 @@ def recipe(id):
       </body>
     </html>
     '''
-# ----------------------------------------------
+# -----------Changing to render templates BELOW-----
 
 from flask import Flask, render_template
 from helper import recipes, descriptions, ingredients, instructions
@@ -41,10 +41,47 @@ app = Flask(__name__)
 def index():
   
   #### Returning a rendered index.html file
-  return ""
+  return render_template("index.html")
 
 @app.route("/recipe/<int:id>")
 def recipe(id):
     
   #### Returning a rendered fried_egg.html file
-  return ""
+  return render_template("fried_egg.html")
+
+# ----------- new render templated html file below ----------
+
+<!DOCTYPE html>
+<html>
+  <body>
+    <a href="/">Back To Recipe List</a>
+    <h1>{{ template_recipe }}</h1>
+    <p>{{ template_description }}</p>
+    <h3>Ingredients</h3>
+    <ul>
+      <!-- Ingredients list elements
+      should fill the <li> tags -->
+      <li>{{template_ingredients[0]}}</li>
+      <li>{{template_ingredients[1]}}</li>
+      <li>{{template_ingredients[2]}}</li>
+    </ul>
+  </body>
+</html>
+
+# ----------- Updated py file below ---------------
+
+from flask import Flask, render_template
+from helper import recipes, descriptions, ingredients
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template("index.html")
+
+@app.route("/recipe/<int:id>")
+def recipe(id):
+  #### Added template variables as 
+  #### variable assignment arguments
+  return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id], template_ingredients=ingredients[id])
+
