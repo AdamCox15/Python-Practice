26b138b66023b8df404e916e7d89b45cb897c21f
Adam Cox <adamcox.dev@gmail.com>
2023-01-17 22:47:03 -0700

    Created commentForm class

--- forms.py
+++ forms.py
@@ -1,26 +1,29 @@
 # ------------ Flask forms practice --------------
-# ------- Get/Post requests and request forms ------------
+# ------- Get/Post requests and request forms/ FlaskForm Class ------------
+# --------- app.py ----------------
 
 from flask import Flask, render_template, request
-from helper import recipes, descriptions, ingredients, instructions, add_ingredients, add_instructions
+from helper import recipes, descriptions, ingredients, instructions, add_ingredients, add_instructions, comments
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "mysecret"
+
+# Created form class 
+class CommentForm(FlaskForm):
+  comment = StringField("Comment")
+  submit = SubmitField("Add Comment")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-  new_id = len(recipes) + 1
-  if len(request.form) > 0:
-    recipes[new_id] = request.form["recipe"]
-    descriptions[new_id] = request.form["description"]
-    new_ingredients = request.form["ingredients"]
-    new_instructions = request.form["instructions"]
-    add_ingredients(new_id, new_ingredients)
-    add_instructions(new_id, new_instructions)
   return render_template("index.html", template_recipes=recipes)
 
-@app.route("/recipe/<int:id>")
+@app.route("/recipe/<int:id>", methods=["GET", "POST"])
 def recipe(id):
-  return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id])
+  # Instantiated form class 
+  comment_form = CommentForm()
+  return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id], template_comments=comments[id], template_form=comment_form)
 
 @app.route("/about")
 def about():
@@ -28,7 +31,7 @@ def about():
 
 # ------------ Route Selection -------------------
 
-# ------------ Updated Base HTML -------
+# ------------ Updated base.html -------
 
 <!DOCTYPE html>
 <html>
@@ -42,7 +45,7 @@ def about():
   </body>
 </html>
 
-# ------------ Updated Index HTML --------------
+# ------------ Updated index.html --------------
 
 {% extends "base.html" %}
 {% block content %}
