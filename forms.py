9f3a78b94adac644938ddc84d679b8752c399172
Adam Cox <adamcox.dev@gmail.com>
2023-01-17 23:37:29 -0700

    added a redirect and a few other things

--- forms.py
+++ forms.py
@@ -2,32 +2,65 @@
 # ------- Get/Post requests and request forms/ FlaskForm Class ------------
 # --------- app.py ----------------
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from helper import recipes, descriptions, ingredients, instructions, add_ingredients, add_instructions, comments
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from forms import RecipeForm, CommentForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysecret"
 
-# Created form class 
-class CommentForm(FlaskForm):
-  comment = StringField("Comment")
-  submit = SubmitField("Add Comment")
-
 @app.route("/", methods=["GET", "POST"])
 def index():
-  return render_template("index.html", template_recipes=recipes)
+  recipe_form = RecipeForm(csrf_enabled=False)
+  if recipe_form.validate_on_submit():
+    new_id = len(recipes)+1
+    recipes[new_id] = recipe_form.recipe.data
+    types[new_id] = recipe_form.recipe_type.data
+    descriptions[new_id] = recipe_form.description.data
+    new_ingredients = recipe_form.ingredients.data
+    new_instructions = recipe_form.instructions.data
+    add_ingredients(new_id, new_ingredients)
+    add_instructions(new_id, new_instructions)
+    comments[new_id] = 
+        return redirect(url_for("recipe", id=new_id, _external=True, _scheme='https'))
+  return render_template("index.html", template_recipes=recipes, template_form=recipe_form)
 
 @app.route("/recipe/<int:id>", methods=["GET", "POST"])
 def recipe(id):
   # Instantiated form class 
-  comment_form = CommentForm()
+  comment_form = CommentForm(csrf_enabled=False)
+  # appended data from comment form to comments list  
+  if True:
+    new_comment = comment_form.comment.data
+    comments[id].append(new_comment)
   return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id], template_comments=comments[id], template_form=comment_form)
 
 @app.route("/about")
 def about():
   return render_template("about.html")
+
+#  ------ forms.py ------------------------
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, RadioField
+from wtforms.validators import DataRequired
+
+class RecipeForm(FlaskForm):
+  recipe_categories = [("Breakfast","Breakfast"), ("Lunch","Lunch"), ("Dinner","Dinner")]
+  recipe = StringField("Recipe", validators=[DataRequired()])  
+  #### Add `recipe_type` and assign it a new radio field instance
+  recipe_type = RadioField("Type", choices=recipe_categories)
+  description = StringField("Description")
+  ingredients = TextAreaField("Ingredients")
+  instructions = TextAreaField("Instructions")
+  submit = SubmitField("Add Recipe")
+
+class CommentForm(FlaskForm):
+  comment = StringField("Comment", validators=[DataRequired()])
+  submit = SubmitField("Add Comment")
 
 # ------------ Route Selection -------------------
 
