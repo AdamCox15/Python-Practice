c3bbb2fccf6f1754a4701485f46619a4afabb0bc
Adam Cox <adamcox.dev@gmail.com>
2023-02-01 20:58:46 -0700

    Practice using .filter

--- sqlAlchemy.py
+++ sqlAlchemy.py
@@ -161,5 +161,28 @@ print("\nCheckpoint 3: fetch the reader who owns the annotation with `id = 331.`
 author_331 = Annotation.query.get(331).author
 print(author_331)
 
+#  ------- Databases in Flask - Reading, Updating and Deleting "filtering" -------
 
+from app import Book, Reader, Review, Annotation
 
+#selected books from the year 2020
+book_2020 = Book.query.filter(Book.year == 2020).all()
+print("All the suggested books in the year 2020:")
+[print(book) for book in book_2020]
+
+book_2020_first = Book.query.filter(Book.year == 2020).first()
+print("\nThe first book fetched from the year 2020: ", book_2020_first)
+
+#specify multiple criteria for filtering
+rev_3_boook13 = Review.query.filter(Review.stars <= 3, Review.book_id == 13).all()
+print("\nThe review of 3 stars or lower written for a book with id = 13: ", rev_3_boook13)
+
+#fetching all the readers with "Adams" surname
+adams = Reader.query.filter(Reader.surname == 'Adams').all()
+[print(person) for person in adams]
+
+#fetching the first book dating prior to the year 2019
+book_pre2019 = Book.query.filter(Book.year <= 2019).first()
+print(book_pre2019)
+
+#  ----------
