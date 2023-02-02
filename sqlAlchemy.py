48331cca975579de5be8ff1f720b586226711735
Adam Cox <adamcox.dev@gmail.com>
2023-02-01 21:16:04 -0700

    practiced advanced filtering

--- sqlAlchemy.py
+++ sqlAlchemy.py
@@ -185,4 +185,35 @@ adams = Reader.query.filter(Reader.surname == 'Adams').all()
 book_pre2019 = Book.query.filter(Book.year <= 2019).first()
 print(book_pre2019)
 
-#  ----------
+#  ---------- More advanced filtering -----------------
+
+from app import db, Book, Reader, Review
+
+#retrieved all reader with .edu e-mails
+education = Reader.query.filter(Reader.email.endswith('edu')).all()
+print(education)
+
+#retrieved all readers with e-mails that contain a . before the @ symbol
+emails = Reader.query.filter(Reader.email.like('%.%@%')).all()
+print("\nReaders with e-mails having a . before the @ symbol:")
+for e in emails:
+  print(e.email)
+
+#ordered all books by year below
+ordered_books = Book.query.order_by(Book.year).all()
+print("\nBooks ordered by year:")
+for book in ordered_books:
+  print(book.title, book.year)
+
+print("\nCheckpoint 1: your code here below:")
+s_names = Reader.query.filter(Reader.surname.endswith('s')).all()
+print(s_names)
+
+print("\nCheckpoint 2: your code here below:")
+sample_emails = Reader.query.filter(Reader.email.like('%@sample%')).all()
+print(sample_emails)
+
+print("\nCheckpoint 3: your code here below:")
+ordered_reviews = Review.query.order_by(Review.stars).all()
+print(ordered_reviews)
+
