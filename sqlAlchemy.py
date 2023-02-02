e25cb72f5eddee7241df4d69cdaa012c81f555f6
Adam Cox <adamcox.dev@gmail.com>
2023-02-01 19:38:45 -0700

    practice with entries: entities

--- sqlAlchemy.py
+++ sqlAlchemy.py
@@ -84,3 +84,21 @@ $ python3
 
 #  ---- Creating database entries: entities -----------
 
+from app import Reader, Book, Review
+
+b1 = Book(id = 123, title = 'Demian', author_name = "Hermann", author_surname = 'Hesse', month = "February", year = 2020)
+r1 = Reader(id = 342, name = 'Ann', surname = 'Adams', email = 'ann.adams@example.com')
+
+print("My first reader:", r1.name)
+
+b2 = Book(id= 533, title = 'The Stranger', author_name = 'Albert', author_surname = 'Camus', month = 'April', year = 2019)
+#Checkpoint 2: 
+r2 = Reader(id = 765, name = 'Sam', surname = 'Adams', email = 'sam.adams@example.com')
+
+print(b2.author_surname)
+
+print(len(r2.email))
+
+
+
+
