962956438c6b54331c305e090cf0db8c87b8f801
Adam Cox <adamcox.dev@gmail.com>
2023-02-02 20:57:56 -0700

    practice hashing

--- sqlAlchemy.py
+++ sqlAlchemy.py
@@ -268,3 +268,22 @@ class Playlist(db.model):
   id = db.Column(db.Integer, primary_key = True)
   playlist_id = db.Column(db.Integer, db.ForeignKey('playlist.id'))
   items = db.relationship('Item', backref = 'playlist', lazy = 'dynamic')
+
+# --------- Intro to Hashing ----------
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+hardcoded_password_string = "123456789_bad_password"
+
+hashed_password = generate_password_hash(hardcoded_password_string)
+print(hashed_password)
+
+password_attempt_one = "abcdefghij_123456789"
+
+hash_match_one = check_password_hash(hashed_password, password_attempt_one)
+print(hash_match_one)
+
+password_attempt_two = "123456789_bad_password"
+
+hash_match_two = check_password_hash(hashed_password, password_attempt_two)
+print(hash_match_two)
