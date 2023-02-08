13390e69283d09f79071c7e8c32dd0a2d357ffe8
Adam Cox <adamcox.dev@gmail.com>
2023-02-07 21:02:17 -0700

    imported login_required @login_required decorator

--- flask.py
+++ flask.py
@@ -54,7 +54,48 @@ login_manager.init_app(app)
 def hello_world():
     return 'Hello, Authentication World!'
 
+# Protecting Pages by importing login_required and using the @login_required decorator
 
+import flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_required
+from flask import request, render_template, flash, redirect,url_for
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = flask.Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+db = SQLAlchemy(app)
+
+class User(UserMixin,db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(64), index=True, unique=True)
+  email = db.Column(db.String(120), index=True, unique=True)
+  password_hash = db.Column(db.String(128))
+  posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+  def __repr__(self):
+    return '<User {}>'.format(self.username)
+
+  def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
+
+  def check_password(self, password):
+    return check_password_hash(self.password_hash, password) 
+
+@login_manager.user_loader
+def load_user(id):
+  return User.query.get(int(id))
+    
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/home')
+@login_required
+def home():
+	return render_template('logged_in.html')
 
 
 
