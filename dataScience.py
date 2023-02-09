acf0682ea63ce5e2ea4c56ccf7a8ffe6aad84ebf
Adam Cox <adamcox.dev@gmail.com>
2023-02-08 21:41:26 -0700

    practice float, strings, loading DataFrame

--- dataScience.py
+++ dataScience.py
@@ -3,3 +3,29 @@
 import numpy as np
 import statsmodels as sm
 import seaborn as sns
+
+# ---- Creating a float -----
+
+bayes_age = 4.0
+print(bayes_age)
+
+#  ---- Creating strings -----
+
+favorite_toy = "Mr. Squeaky"
+owner = 'DataCamp'
+
+#  --- Correcting string errors -----
+
+birthday = "2017-07-14'
+case_id = 'DATACAMP!123-456?
+# fixed below 
+birthday = '2017-07-14'
+case_id = 'DATACAMP!123-456?'
+
+#  ---- Loading the DataFrame ------
+
+import pandas as pd
+
+r = pd.read_csv('ransom.csv')
+
+print(r)
