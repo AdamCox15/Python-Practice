8adb0a9052412af2df72892e43ffe0a23b5868c9
Adam Cox <adamcox.dev@gmail.com>
2023-02-13 18:11:06 -0700

    tested for significance

--- dataAnalystCodecademy.py
+++ dataAnalystCodecademy.py
@@ -43,3 +43,26 @@ plt.title('Why do citizens visit our website?')
 plt.axis('equal')
 
 plt.show()
+
+# ------- Hypothesis Testing for A/B Tests "testing for significance" -------
+
+from scipy.stats import chi2_contingency
+
+control_group = 7700
+variant_group = 7700
+control_success = 231
+variant_success = 308
+control_fail = control_group - control_success
+variant_fail = variant_group - variant_success
+
+results = chi2_contingency([
+  [control_fail, control_success],
+  [variant_fail, variant_success]
+])
+
+p = results[1]
+
+if p < 0.05:
+    print("Significant!")
+if p > 0.05:
+    print("Not significant!")
