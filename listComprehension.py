f99b1f8a6d696913dba6f1d7251344d56cfebd2d
Adam Cox <adamcox.dev@gmail.com>
2023-02-14 20:56:57 -0700

    Product, grater than, First only, zip

--- listComprehension.py
+++ listComprehension.py
@@ -39,6 +39,31 @@ opposite = [not bool for bool in booleans]
 names = ["Elaine", "George", "Jerry", "Cosmo"]
 is_Jerry = [name == "Jerry" for name in names]
 
+# Greater than two
+nums = [5, -10, 40, 20, 0]
+greater_than_two = [num > 2 for num in nums]
 
+# Product ---- new list, multiple items from sub lists
+nested_lists = [[4, 8], [15, 16], [23, 42]]
+product = [item1 * item2 for (item1, item2) in nested_lists]
+print(product)
+
+# Greater than
+nested_lists = [[4, 8], [16, 15], [23, 42]]
+greater_than = [item1 > item2 for (item1, item2) in nested_lists]
+
+# First Only prints only the first item in a sub-list
+nested_lists = [[4, 8], [16, 15], [23, 42]]
+first_only = [item[0] for item in nested_lists]
+
+# Add with a zip
+a = [1.0, 2.0, 3.0]
+b = [4.0, 5.0, 6.0]
+sums = [item1 + item2 for (item1, item2) in zip(a, b)]
+
+# Divide with zip
+a = [1.0, 2.0, 3.0]
+b = [4.0, 5.0, 6.0]
+quotients = [item1 / item2 for (item2, item1) in zip(a,b)]
 
 
