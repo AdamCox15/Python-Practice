3c5f246966a0af91fcc719d47d59c9a8964ed7ee
Adam Cox <adamcox.dev@gmail.com>
2023-02-14 20:05:10 -0700

    Practicing list comprehension

---
+++ listComprehension.py
@@ -0,0 +1,9 @@
+#  ------- List Comprehension practice ----------------
+
+nums = [4, 8, 15, 16, 23, 42]
+double_nums = [num * 2 for num in nums]
+
+# Squared nums
+nums = range(11)
+squares = [num**2 for num in nums]
+
